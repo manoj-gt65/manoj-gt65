066ae1b865d5b2e6d5878a10ad26124ba4064270
MANOJ M <manojofficial2705@gmail.com>
2026-04-21 23:51:42 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from sensors.motion_sensor import detect_motion
 motion = detect_motion()
 
 if motion:
-    print("Light ON")
+    print("🚶 Motion Detected → Light ON")
 else:
-    print("Light DIM")
+    print("🌙 No Motion → Light DIM")
